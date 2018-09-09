bade57520d24d9da188ed8a626862243b9e4cc5f
Eduardo Pascua <2377749+khelben5@users.noreply.github.com>
2018-09-09 19:03:56 +0200

    Implement markers over the map.

--- app/src/main/java/com/eduardodev/cars/model/Car.kt
+++
@@ -1,5 +0,0 @@
-package com.eduardodev.cars.model
-
-
-class Car {
-}
--- app/src/main/java/com/eduardodev/cars/presentation/car/map/CarMapFragment.kt
+++ app/src/main/java/com/eduardodev/cars/presentation/car/map/CarMapFragment.kt
@@ -1,13 +1,23 @@
 package com.eduardodev.cars.presentation.car.map
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.eduardodev.cars.R
+import com.eduardodev.cars.presentation.car.set.CarSetViewModel
+import com.eduardodev.cars.presentation.model.Car
+import com.eduardodev.cars.presentation.model.Resource
+import com.eduardodev.cars.presentation.model.Success
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.MarkerOptions
 
 
 class CarMapFragment : Fragment() {
@@ -15,6 +25,10 @@ class CarMapFragment : Fragment() {
     companion object {
         fun newInstance() = CarMapFragment()
     }
+
+    private val model by lazy { ViewModelProviders.of(activity!!)[CarSetViewModel::class.java] }
+    private var map: GoogleMap? = null
+    private var cars: List<Car>? = null
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -24,11 +38,46 @@ class CarMapFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        model.cars.observe(this, Observer { resource ->
+            resource?.let {
+                onResourceLoaded(resource)
+            }
+        })
         (childFragmentManager.findFragmentById(R.id.carMapFragment) as SupportMapFragment)
-                .getMapAsync(::setupMap)
+                .getMapAsync(::onMapLoaded)
     }
 
-    private fun setupMap(map: GoogleMap) {
+    private fun onMapLoaded(map: GoogleMap) {
+        this.map = map
+        this.cars?.let { updateMarkers(map, it) }
+    }
 
+    private fun onResourceLoaded(resource: Resource) {
+        if (resource !is Success) return
+        val cars = (resource.result as List<*>).map { it as Car }
+        this.cars = cars
+        this.map?.let { updateMarkers(it, cars) }
+    }
+
+    private fun updateMarkers(map: GoogleMap, cars: List<Car>) {
+        val allPositions = ArrayList<Pair<Double, Double>>(cars.size)
+        cars.map {
+            allPositions.add(it.position.first to it.position.second)
+            map.addMarker(
+                    MarkerOptions()
+                            .position(LatLng(it.position.first, it.position.second))
+                            .title(getString(
+                                    R.string.car_list_item_main_description_template,
+                                    it.model,
+                                    it.plate
+                            ))
+            )
+        }
+        map.moveCamera(CameraUpdateFactory.newLatLngBounds(
+                LatLngBounds.Builder().apply {
+                    allPositions.forEach { include(LatLng(it.first, it.second)) }
+                }.build(),
+                resources.getDimensionPixelSize(R.dimen.map_padding)
+        ))
     }
 }
